0b9aaf2e4fff2000d2ce6e993986dc04a7cee665
xionng <leeso0717@naver.com>
2024-07-29 02:54:02 +0900

    add: 로딩바 컴포넌트 추가

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -4,6 +4,7 @@ import greenicon from '../../assets/img/GreenIcon.png';
 import yellowicon from '../../assets/img/YellowIcon.png';
 import redicon from '../../assets/img/RedIcon.png';
 import shadowicon from '../../assets/img/Shadow.png';
+
 function Loading() {
   return (
     <>
@@ -20,12 +21,15 @@ function Loading() {
             <ShadowIcon src={shadowicon} alt="그림자 아이콘" />
           </ShadowContainer>
         </ImgContainer>
-        <BarContainer></BarContainer>
+        <BarContainer>
+          <FixedBar />
+          <MovingBar />
+        </BarContainer>
         <MentContainer>
           <NameM>
             <GreenSpan>홍길동님의 하루</GreenSpan>를 분석하는 중입니다
           </NameM>
-          <WaitM>조금만 기다려주세요:)</WaitM>
+          <WaitM>조금만 기다려주세요 :)</WaitM>
         </MentContainer>
       </LoadingContainer>
     </>
@@ -46,17 +50,16 @@ const LoadingContainer = styled.div`
 const ImgContainer = styled.div``;
 const FaceContainer = styled.div`
   height: 13rem;
-  /* background-color: red; */
   display: flex;
   justify-content: center;
   align-items: end;
 `;
 const ShadowContainer = styled.div`
-  /* background-color: red; */
   display: flex;
   justify-content: center;
   align-items: end;
 `;
+
 const moveUpDown = keyframes`
   0%, 100% {
     transform: translateY(1rem);
@@ -73,17 +76,51 @@ const FaceIcon = styled.img`
   animation: ${moveUpDown} 1.5s infinite;
   animation-delay: ${(props) => props.delay};
 `;
+
 const ShadowIcon = styled.img`
   width: 5.8rem;
   height: 1rem;
   margin: 0rem 3.7rem;
 `;
 
-const BarContainer = styled.div``;
+const BarContainer = styled.div`
+  position: relative;
+  width: 53.6rem;
+  height: 3.4rem;
+  margin: 3.3rem;
+`;
+
+const FixedBar = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 3rem;
+  background: #f4f1da;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const moveRight = keyframes`
+  0% {
+    width: 0rem;
+  }
+  100% {
+    width: 53.6rem;
+  }
+`;
+
+const MovingBar = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  border-radius: 3rem;
+  background: #e47f69;
+  animation: ${moveRight} 6s forwards;
+`;
 
 const MentContainer = styled.div`
   text-align: center;
   line-height: 3.5rem;
+  /* margin-top: 3.3rem; */
 `;
 
 const NameM = styled.p`
