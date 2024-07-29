b03b5ae09bea2fa3726c1c4f9190a612ea785433
Patrick-Gong <owlcountry01@gmail.com>
2024-07-29 21:38:23 +0900

    chore: uuid 추가 &  Noto Sans KR 폰트 추가

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -18,8 +18,8 @@ const GlobalStyles = createGlobalStyle`
     }
     body{
         line-height: 1;
+        background-color: #Ffffff;
         font-family: 'Noto Sans KR', sans-serif;
-        background-color: #F6F9F0;
     }
     ol, ul{
         list-style: none;
